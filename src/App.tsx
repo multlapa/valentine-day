796c27938307ef8aa39a554c17d19168282d5362
Daniil <multlapa@yandex.ru>
2026-02-02 23:20:44 +0300

    Move project to root

--- src/App.tsx
+++ src/App.tsx
@@ -138,7 +138,7 @@ function App() {
           </MainBlock>
           ) : (
             <BlockCat>
-              <div class="tenor-gif-embed" data-postid="23625303" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/cat-jump-happy-jumping-for-joy-jumping-gif-23625303">Cat Jump Sticker</a>from <a href="https://tenor.com/search/cat-stickers">Cat Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+              <div className="tenor-gif-embed" data-postid="23625303" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/cat-jump-happy-jumping-for-joy-jumping-gif-23625303">Cat Jump Sticker</a>from <a href="https://tenor.com/search/cat-stickers">Cat Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
             </BlockCat>
           )}
       </Container>
